75181161d543f23573ee973e4a14b708bfff37ab
Simon D. <simon.doehl@googlemail.com>
2024-11-21 10:40:07 +0100

    batch generation and grid returns

--- diffusion.py
+++ diffusion.py
@@ -1,19 +1,40 @@
 import torch
-from diffusers import AutoPipelineForText2Image
+from diffusers import AutoPipelineForText2Image, DiffusionPipeline
+from PIL import Image
+import numpy as np
 
 class diffuser():
-    def __init__(self):
-        self.set_pipeline("stabilityai/stable-diffusion-xl-base-1.0")
+    def __init__(self, model = "stabilityai/stable-diffusion-xl-base-1.0"):
+        self.set_pipeline(model)
 
-    def generate(self, prompt, steps = 25, guidance = 7):
-        return self.pipe(prompt,
+
+    def generate(self, prompt, steps = 25, guidance = 7, batch_size = 1):
+        self.images = self.pipe(prompt * batch_size,
                          num_inference_steps = steps,
                          guidance_scale = guidance,
                         ).images
+        return self.images
+
 
     def set_pipeline(self, model):
-        self.pipe = AutoPipelineForText2Image.from_pretrained(
+        self.model = model
+        self.pipe = DiffusionPipeline.from_pretrained(
             model,
             torch_dtype=torch.float16)
+
+        self.pipe.enable_vae_slicing()
         self.pipe = self.pipe.to("cuda")
         self.pipe.enable_model_cpu_offload()
+
+
+    def get_grid(self):
+        size = int(np.ceil(np.sqrt(len(self.images))))
+        return self.image_grid(self.images, size, size)
+
+
+    def image_grid(self, imgs, rows, cols):
+        w, h = imgs[0].size
+        grid = Image.new('RGB', size=(cols*w, rows*h))
+
+        for i, img in enumerate(imgs): grid.paste(img, box=(i%cols*w, i//cols*h))
+        return grid

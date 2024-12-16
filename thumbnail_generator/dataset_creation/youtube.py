a1fc135b27d9185056bc6a1b199940d8a99f2084
Simon D. <simon.doehl@googlemail.com>
2024-12-16 17:58:32 +0100

    thumbnail description generation and increased dataset

--- thumbnail_generator/dataset_creation/youtube.py
+++ thumbnail_generator/dataset_creation/youtube.py
@@ -3,10 +3,13 @@ from youtube_transcript_api import YouTubeTranscriptApi
 import json
 import requests, shutil
 import os
+import asyncio
 from PIL import Image
 from pathlib import Path
 from pytube.innertube import _default_clients
-from IPython.display import clear_output
+from IPython.display import clear_output, display
+import google.generativeai as genai
+from google.api_core.exceptions import InternalServerError
 
 _default_clients["ANDROID_MUSIC"] = _default_clients["WEB"]
 yt_str = "https://www.youtube.com/watch?v={}"
@@ -46,8 +49,12 @@ class Youtube():
                 try:
                     v["caption"] = YouTubeTranscriptApi.get_transcript(v["id"])
                     added.append(v)
+                    try:
+                        clear_output(wait=True)
+                        print(v["caption"])
+                    except: pass
                 except Exception as e:
-                    if verbose: print(e)
+                    v["caption"] = None
         return added
 
     # attempt to use pytube instead of YoutubeTranscriptApi
@@ -88,3 +95,61 @@ class Youtube():
                         display(img)
                     except: pass
         return thumbnails
+
+    # def add_thumbnail_description(self, api_key, amount = 15, show = False, requests_per_min = 15):
+    #     genai.configure(api_key=api_key)
+    #     model = genai.GenerativeModel('gemini-1.5-flash')
+    #     prompt = "Generate a positive prompt for Stable Diffusion for the given thumbnail with no more than 77 tokens"
+    #     for v in self.videos:
+    #         try: v["thumbnail_description"]; continue
+    #         except: pass
+    #         try: image = Image.open(os.path.join(self.path, os.path.join(self.path, f"{v["id"]}.webp")))
+    #         except:
+    #             print(f"Video {v["id"]} has no thumbnail")
+    #             continue
+    #         v["thumbnail_description"] = model.generate_content([image, prompt]).text
+    #         if show:
+    #             try:
+    #                 clear_output(wait=True)
+    #                 display(image)
+    #                 display(v["thumbnail_description"])
+    #             except: pass
+
+    async def add_thumbnail_description(self, api_key, amount=15, show=False, requests_per_min=10, overwrite=False):
+        async def generate_thumbnail_description(video):
+            try:
+                video["thumbnail_description"]
+                if not overwrite: return -1
+            except: pass
+            id = video['id']
+            image_path = os.path.join(self.path, f"{id}.webp")
+            try: image = Image.open(image_path)
+            except FileNotFoundError:
+                print(f"Video {id} has no thumbnail")
+                return -1
+
+            prompt = "Generate a positive prompt for Stable Diffusion for the given thumbnail with no more than 77 tokens. The response should only include the prompt"
+            for i in range(10):
+                try: video["thumbnail_description"] = model.generate_content([image, prompt]).text
+                except InternalServerError: continue
+                break
+            if show:
+                try:
+                    clear_output(wait=True)
+                    display(image)
+                    print(video["thumbnail_description"])
+                except Exception as e: pass
+
+        genai.configure(api_key=api_key)
+        model = genai.GenerativeModel('gemini-1.5-flash')
+        interval = 60 / requests_per_min
+        semaphore = asyncio.Semaphore(requests_per_min)
+        reqs = 0
+        for v in self.videos:
+            if amount is not None and amount < 0: break
+            async with semaphore:
+                ret = await generate_thumbnail_description(v)
+                if ret != -1: await asyncio.sleep(interval)
+            reqs += 1
+            print(reqs)
+            if amount is not None: amount -= 1
